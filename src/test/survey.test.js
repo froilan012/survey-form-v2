5d0af195f6c797bded6be6597052b94f68257675
froilan012 <froilan.i.fajardo@gmail.com>
2023-07-19 10:41:00 +0800

    removed intentional failed test

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -22,8 +22,4 @@ describe("Survey Model", function(){
 
         expect(result).to.equal("Error! Captcha input doesn't matched.");
     });
-
-    it('add intentional failed test', function() {
-        expect(true).to.equal(false);
-    });
 });
