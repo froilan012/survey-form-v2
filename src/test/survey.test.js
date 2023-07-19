e0dd3046be6dbd487501c3e7b7aaee414739363e
froilan012 <froilan.i.fajardo@gmail.com>
2023-07-19 10:24:35 +0800

    add testing123456789

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -11,7 +11,7 @@ describe("Survey Model", function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
-        console.log('testing123456789');
+        console.log('testing');
 
         expect(result).to.equal("Success! Captcha input matched.");
     });
